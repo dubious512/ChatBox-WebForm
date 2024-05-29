4fc51336cab0bc8569a8ab11ee8fc17b22f58fa3
Nguyễn Cường <cuong2k3@gmail.com>
2024-05-29 21:38:43 +0700

    thêm Mapping

---
+++ WebApplication1/Mappings/RoomProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebApplication1.Data.Entities;
+using WebApplication1.Models;
+
+namespace WebApplication1.Mappings
+{
+    public class RoomProfile : Profile
+    {
+        public RoomProfile ()
+        {
+            CreateMap<Room, RoomViewModel>();
+            CreateMap<RoomViewModel, Room>();
+        }
+    }
+}
---
+++ WebApplication1/Mappings/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebApplication1.Data.Entities;
+using WebApplication1.Models;
+namespace WebApplication1.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<ManageUser, UserViewModel>()
+                .ForMember(dst => dst.UserName, otp => otp.MapFrom(x => x.UserName));
+            CreateMap<UserViewModel, ManageUser>();
+        }
+    }
+}
